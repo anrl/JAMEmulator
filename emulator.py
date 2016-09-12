1e1e2376f55e71bd94d083d4ffb1a34ebdda958a
Andrew Gowing <gowing.andrew@gmail.com>
2016-09-12 17:39:25 -0400

    Add example.yaml

--- emulator.py
+++ emulator.py
@@ -1,17 +1,39 @@
 #!/usr/bin/python
 
-from sys import argv
+from sys import argv, exit
 from yaml import load
+from select import poll, POLLIN
+from subprocess import Popen, PIPE
 
 from mininet.net import Mininet
-from mininet.cli import CLI
+from mininet.topo import Topo
 from mininet.log import setLogLevel
 from mininet.node import CPULimitedHost
 from mininet.link import TCLink
-from mininet.node import Node
 
-from ethernet import Ethernet
-
+class Ethernet(Topo):
+    def build(self, config, isWiFi=True):
+        if ('cloud' in config): 
+            for i in config['cloud']:
+                cpu = i['cpu'] if 'cpu' in i else 1.0
+                self.addHost(i['name'], cpu=cpu)
+        if ('fog' in config): 
+            for i in config['fog']:
+                cpu = i['cpu'] if 'cpu' in i else 1.0
+                self.addHost(i['name'], cpu=cpu)
+        if ('device' in config):
+            for i in config['device']:
+                cpu = i['cpu'] if 'cpu' in i else 1.0
+                self.addHost(i['name'], cpu=cpu)
+        if ('switch' in config):
+            for i in config['switch']:
+                self.addSwitch(i['name'])
+        if ('link' in config):
+            for i in config['link']:
+                bw = i['bw'] if 'bw' in i else 100
+                delay = i['delay'] if 'delay' in i else '0ms'
+                loss = i['loss'] if 'loss' in i else 0
+                self.addLink(i['node1'], i['node2'], bw=bw, delay=delay, loss=loss)
 
 def checkDupEth(config):
     hosts = list()
@@ -34,40 +56,86 @@ def checkDupEth(config):
         if nodes.count(i) > 1:
             raise RuntimeError("Invalid topology: %s has duplicate eth interface" %i)
 
-def addWiFi(net, config):    
-    if ('station' in config):
-        for i in config['station']:
-            net.addStation(i['name'])
-    if ('baseStation' in config):
-        for i in config['baseStation']:
-           net.addBaseStation(i['name'])
+def monitorFiles(outfiles, timeoutms):
+    "Monitor set of files and return [(host, line)...]"
+    devnull = open( '/dev/null', 'w' )
+    tails, fdToFile, fdToHost = {}, {}, {}
+    
+    for h, outfile in outfiles.iteritems():
+        tail = Popen(['tail', '-f', outfile],
+                      stdout=PIPE, stderr=devnull)
+        fd = tail.stdout.fileno()
+        tails[h] = tail
+        fdToFile[fd] = tail.stdout
+        fdToHost[fd] = h
+    # Prepare to poll output files
+    readable = poll()
+    
+    for t in tails.values():
+        readable.register(t.stdout.fileno(), POLLIN)
+    try:    
+        while True:
+            fdlist = readable.poll(timeoutms)
+            if fdlist:
+                for fd, _flags in fdlist:
+                    f = fdToFile[fd]
+                    host = fdToHost[fd]
+                    # Wait for a line of output
+                    line = f.readline().strip()
+                    yield host, line
+            else:
+                # If we timed out, return nothing
+                yield None, ''
+    except KeyboardInterrupt:
+        for t in tails.values():
+            t.terminate()
+        devnull.close()  # Not really necessary
 
-if __name__ == '__main__':
+if __name__ == '__main__':    
+    if len(argv) < 2: 
+        print "usage: sudo python emulator.py config.yaml"
+        exit()
     script, filename = argv
     data = open(filename)
     config = load(data)
+
     checkDupEth(config)
     setLogLevel('info')
     topo = Ethernet(config) 
     net = Mininet(topo, host=CPULimitedHost, link=TCLink)
     net.addNAT().configDefault()
     net.start()
-    #addWiFi(net,config)
+    outfiles, errfiles = {}, {}
 
     if ('cloud' in config): 
         for i in config['cloud']:
             if ('cmd' in i):
-                host = net.get(i['name'])
-                host.cmd(i['cmd'])
+                h = net.get(i['name'])
+                outfiles[h] = '/tmp/%s.out' % h.name
+                errfiles[h] = '/tmp/%s.err' % h.name
+                h.cmd('echo >', outfiles[h])
+                h.cmd('echo >', errfiles[h])
+                h.cmdPrint(i['cmd'], '>', outfiles[h], '2>', errfiles[h], '&')
     if ('fog' in config): 
         for i in config['fog']:
             if ('cmd' in i):
-                host = net.get(i['name'])
-                host.cmd(i['cmd']) 
+                h = net.get(i['name'])
+                outfiles[h] = '/tmp/%s.out' % h.name
+                errfiles[h] = '/tmp/%s.err' % h.name
+                h.cmd('echo >', outfiles[h])
+                h.cmd('echo >', errfiles[h])
+                h.cmdPrint(i['cmd'], '>', outfiles[h], '2>', errfiles[h], '&')
     if ('device' in config):
         for i in config['device']:
             if ('cmd' in i):
-                host = net.get(i['name'])
-                host.cmd(i['cmd']) 
-    CLI(net)
+                h = net.get(i['name'])
+                outfiles[h] = '/tmp/%s.out' % h.name
+                errfiles[h] = '/tmp/%s.err' % h.name
+                h.cmd('echo >', outfiles[h])
+                h.cmd('echo >', errfiles[h])
+                h.cmdPrint(i['cmd'], '>', outfiles[h], '2>', errfiles[h], '&')
+
+    for host, line in monitorFiles(outfiles, timeoutms=500):
+        if host:
+            print '%s: %s' % (host.name, line)
     net.stop()
